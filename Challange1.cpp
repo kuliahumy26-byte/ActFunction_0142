5ebf286f7b75dd9eddf5e804ee3e473981875e39
kuliahumy26-byte <kuliahumy26@gmail.com>
2026-03-03 20:19:00 +0700

    Prosedur commit

--- Challange1.cpp
+++ Challange1.cpp
@@ -3,3 +3,10 @@ using namespace std;
 
 float var_jarijari;
 float var_phi = 3.14159;
+void first(){
+    cout << "masukkan jarijari pada lingkaran = ";
+    cin >> var_jarijari;
+}
+float luas(float jarijari,float phi){
+    return phi * jarijari * jarijari;
+}
