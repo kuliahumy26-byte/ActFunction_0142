a7138342bc7f22905321782d94d8da19c57490bf
kuliahumy26-byte <kuliahumy26@gmail.com>
2026-03-03 20:10:30 +0700

    Implementasi prosedur

--- Praktikum2.cpp
+++ Praktikum2.cpp
@@ -3,3 +3,10 @@
 using namespace std;
 // deklarasi global
 int p, l, luas;
+// Implementasi Prosedur dan Fungsi
+void input() {
+    cout << "Masukkan panjang = ";
+    cin >> p;
+    cout << "Masukkan lebar = ";
+    cin >> l;
+}
